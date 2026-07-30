1c5872b44c345ab7d0a60c7022152afac59bcf88
霧流羽kiriha <uraume2027@gmail.com>
2026-07-30 11:41:26 +0800

    Update app.c

--- app.c
+++ app.c
@@ -4,18 +4,15 @@
 int main()
 {
     int recursion = 1, click_trig = 1;
-    char content[65535];
     
     while (recursion){
-        if (click_trig){
-            recursion = 1;
-        }
-        else{
+        if !(click_trig){
             recursion = 0;
         }
         printf("(waiting u click specific link...)");
         scanf("%d", &click_trig);
     }
     // flag in the comment:) or else diretory
+    
     return 0;
 }
