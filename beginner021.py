9a161a6ae8b70a51e151c31af0b3ce9af093b503
霧流羽kiriha <uraume2027@gmail.com>
2026-08-04 14:40:59 +0800

    Update beginner021.py

--- beginner021.py
+++ beginner021.py
@@ -1,11 +1,11 @@
 from itertools import combinations as co
 
 
-def validity_of_3_sides(d, e, f) -> bool:
+def validity_of_3_sides(d: int, e: int, f: int) -> bool:
     three_side: list = [d, e, f]
-    if any(x <= 0 for x in three_side):
+    if any(x <= 0 for x in three_side):  # Verify that the side is a positive number.
         return False
-    for a1, a2 in co(three_side, 2):
+    for a1, a2 in co(three_side, 2):  # Verify whether any two sides length are greater than the third side.
         if a1 + a2 <= sum(three_side) - a1 - a2:
             return False
     return True
@@ -17,5 +17,5 @@ if not validity_of_3_sides(a, b, c):
 else:
     s: float = (a + b + c) / 2
     A: float = (s * ((s - a) * (s - b) * (s - c))) ** 0.5
-    check: str = str(A).split('.')[1]
+    check: str = str(A).split('.')[1]  # Judging that is A an integer, u can using is_integer() to improve efficiency.
     print(s, f"{A:.1f}" if any(x in "123456789" for x in check) else int(A), sep='\n')
