0e69a111391b18a7a98c52a60fe1a36646b6b556
霧流羽kiriha <uraume2027@gmail.com>
2026-08-07 13:08:18 +0800

    Update beginner007.c

--- beginner007.c
+++ beginner007.c
@@ -19,14 +19,18 @@ The flag stores the address of a string literal
 So The pointer can be changed
 But the string literal cannot be modified
 */
-    scanf("%s", input);
-    int len = sizeof(rockyou) / sizeof(rockyou[0]);  // Stores the number of strings in the array
-    for (int i = 0;i < len;i++){
-        if (strcmp(input, rockyou[i]) == 0){  // After compare, if same return 0
-            flag = "True";
-            break;
+    int s;
+    scanf("%d", &s);
+    for (int j = 0;j < s;j++){
+        scanf("%s", input);
+        int len = sizeof(rockyou) / sizeof(rockyou[0]);  // Stores the number of strings in the array
+        for (int i = 0;i < len;i++){
+            if (strcmp(input, rockyou[i]) == 0){  // After compare, if same return 0
+                flag = "True";
+                break;
+            }
         }
+        printf("%s\n", flag);
     }
-    printf("%s\n", flag);
     return 0;
 }
