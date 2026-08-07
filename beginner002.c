251cf8f804ed901188ad83e79deb91e7456aaa4b
霧流羽kiriha <uraume2027@gmail.com>
2026-08-07 11:04:05 +0800

    Create beginner002.c

---
+++ beginner002.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+#define p(x) printf(x)
+#define loop(x) for(int i = 0;i < x;i++)
+
+int main()
+{
+    p("#include <iostream>\n");
+    p("using namespace std;\n\n");
+    p("int main(){\n");
+    loop(7){
+        p("     cout << \"Hello World\" << endl;\n");
+    }
+    p("     return 0;\n}");
+    return 0;
+}
